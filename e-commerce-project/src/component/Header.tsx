595eb7b5c041fc22cf8b4fec373c87c96586b5b1
Talo <nghiwewelekwataloshili@gmail.com>
2024-07-01 21:40:13 +0200

    e-commerce-project

--- e-commerce-project/src/component/Header.tsx
+++ e-commerce-project/src/component/Header.tsx
@@ -63,6 +63,17 @@ function Header() {
             </li>
           </ul>
         </div>
+        <form className="d-flex" role="search">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </form>
       </div>
     </nav>
   );
