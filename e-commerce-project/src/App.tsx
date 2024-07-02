c50949529133a3dadf8cb6baa0edff64007ca38d
Talo <nghiwewelekwataloshili@gmail.com>
2024-07-02 16:00:27 +0200

    e-commerce-project

--- e-commerce-project/src/App.tsx
+++ e-commerce-project/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./component/Header";
+import Content from "./component/Content";
 
 function App() {
   return (
     <div>
       <Header />
+      <Content />
     </div>
   );
 }
---
+++ e-commerce-project/src/component/Content.tsx
@@ -0,0 +1,13 @@
+function Content() {
+  return (
+    <div className="container text-center">
+      <div className="row">
+        <div className="col">column</div>
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+      </div>
+    </div>
+  );
+}
+
+export default Content;
