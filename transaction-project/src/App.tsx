726260a4e2c8e5aeb7caadefaa19a863c5b9a4d0
Talo <nghiwewelekwataloshili@gmail.com>
2024-07-11 11:40:53 +0200

    transaction-project

--- transaction-project/src/App.tsx
+++ transaction-project/src/App.tsx
@@ -1,9 +1,18 @@
-import Header from "./component/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Events from "./pages/Events";
+import About from "./pages/About";
 
 function App() {
   return (
     <div>
-      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route index element={<Events />} />
+          <Route index element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ transaction-project/src/pages/Events.tsx
@@ -0,0 +1,12 @@
+import Header from "../component/Header";
+
+function Events() {
+  return (
+    <>
+      <Header />
+      <h1>Events Page</h1>
+    </>
+  );
+}
+
+export default Events;
